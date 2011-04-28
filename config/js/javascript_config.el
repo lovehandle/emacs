90aeb61f3111665400394692f81a009773718d73
Ryan Closner <ryan@crystalcommerce.com>
2011-04-28 14:24:00 -0700

    Require js2-mode and set default offset to 2

--- config/js/javascript_config.el
+++ config/js/javascript_config.el
@@ -1,4 +1,9 @@
+(require 'js2-mode)
+
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
+
+(custom-set-variables
+ '(js2-basic-offset 2))
 
 (provide 'javascript_config)
