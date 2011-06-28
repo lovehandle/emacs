4aeedbb884b7a3934e400c8dba3d6eaff1d526a5
Ryan Closner <ryan@crystalcommerce.com>
2011-06-28 14:02:27 -0700

    Start server on initialization

--- rclosner.el
+++ rclosner.el
@@ -1,6 +1,8 @@
 ;; These should be loaded on startup rather than autoloaded on demand
 ;; since they are likely to be used in every session
 
+(server-start)
+
 (require 'cl)
 (require 'saveplace)
 (require 'ffap)
